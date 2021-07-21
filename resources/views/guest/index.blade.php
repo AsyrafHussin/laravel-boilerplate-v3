515f8b6df12e0ac995fd8ba8b795e0accd8e2e4d
Asyraf Hussin <asyrafhussin4@gmail.com>
2021-07-22 02:29:48 +0800

    Update index page

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master.index')
 
 @section('main')
-    <h1 class="text-center pt-5">Laravel boilerplate v2 <small>by Asyraf Hussin</small></h1>
+    <h1 class="text-center pt-5">Laravel boilerplate v3 <small>by Asyraf Hussin</small></h1>
 @endsection
