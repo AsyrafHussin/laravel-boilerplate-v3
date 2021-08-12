85a49bfcf965ca750978e0a81ced330157ad3c07
Asyraf Hussin <asyrafhussin4@gmail.com>
2021-08-13 04:47:35 +0800

    Add laravel Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Silber\Bouncer\Database\HasRolesAndAbilities;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasRolesAndAbilities, Notifiable;
+    use HasApiTokens,HasFactory, HasRolesAndAbilities, Notifiable;
 
     /**
      * The attributes that are mass assignable.
