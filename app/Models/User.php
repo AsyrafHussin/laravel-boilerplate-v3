bd969a5a5641af0c61a5db1a4feaa011701ce3d3
Asyraf Hussin <asyrafhussin4@gmail.com>
2021-08-26 04:32:35 +0800

    Use PHPDoc comments from base class in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,19 +15,19 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = ['name', 'email', 'password'];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes that should be hidden for serialization.
      *
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attributes that should be cast.
      *
      * @var array
      */
